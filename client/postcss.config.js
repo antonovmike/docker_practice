32afa3c9d6bf4706c87a61e9cb7ff4964403c2f9
antonovmike <antonovma@outlook.com>
2026-01-14 17:31:56 +0100

    [FIX] Add import `@import "tailwindcss";` to `client/src/index.css` to fix tailwind

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,18 @@
+// export default {
+//   plugins: {
+//     "@tailwindcss/postcss": {},
+//     autoprefixer: {},
+//   },
+// };
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss(), autoprefixer()],
 };
+// import { defineConfig } from "vite";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+// });
