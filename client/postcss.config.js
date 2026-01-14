b173527d2f20a5cd03d0cac33caf51a871dab824
antonovmike <antonovma@outlook.com>
2026-01-14 17:36:44 +0100

    [REM] Remove commented code in `client/postcss.config.js`

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,18 +1,6 @@
-// export default {
-//   plugins: {
-//     "@tailwindcss/postcss": {},
-//     autoprefixer: {},
-//   },
-// };
 import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
 
 export default {
   plugins: [tailwindcss(), autoprefixer()],
 };
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   plugins: [tailwindcss()],
-// });
