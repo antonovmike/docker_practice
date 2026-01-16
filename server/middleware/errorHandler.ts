219f7c26579216859cc3c8a4c4207ccc53eddd51
antonovmike <antonovma@outlook.com>
2026-01-16 17:36:35 +0100

    [IMP] Implement res.status(404) and res.status(500)

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,10 +1,12 @@
+import type { Request, Response, NextFunction } from "express";
 // Middleware: 404 handler
 function notFound(_req: Request, res: Response) {
-  // TO DO
+  res.status(404).json({ error: "Not found" });
 }
 // Middleware: Error handler
 function errorHandler(err: Error, _req: Request, res: Response) {
-  // TO DO
+  console.error("Error:", err.message);
+  res.status(500).json({ error: "Internal server error" });
 }
 
 export { notFound, errorHandler };
