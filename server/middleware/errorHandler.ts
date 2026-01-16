8c9ee2c7d7df815541f4884b1dd315584e635bf1
antonovmike <antonovma@outlook.com>
2026-01-16 17:29:26 +0100

    [ADD] Add empty file server/middleware/errorHandler.ts

---
+++ server/middleware/errorHandler.ts
@@ -0,0 +1,10 @@
+// Middleware: 404 handler
+function notFound(_req: Request, res: Response) {
+  // TO DO
+}
+// Middleware: Error handler
+function errorHandler(err: Error, _req: Request, res: Response) {
+  // TO DO
+}
+
+export { notFound, errorHandler };
