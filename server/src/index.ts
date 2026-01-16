9cffd1446b832caa7d98f899f2d5d4091a816642
antonovmike <antonovma@outlook.com>
2026-01-16 08:51:11 +0100

    [REM] Remove commented code in server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,8 +11,3 @@ app.get("/greetings", (_req: Request, res: Response) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-// npm run dev
-// http://localhost:3000/greetings
-// docker build -t hello-server:latest .
-// docker run --rm -p 3000:3000 hello-server:latest
-// Test: curl http://localhost:3000/greetings -> "Hello"
