88262b7ad13a1f16ff978e4b8633e9922bb93971
antonovmike <antonovma@outlook.com>
2026-01-16 17:44:18 +0100

    [IMP] Error handler connected to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
+import { notFound, errorHandler } from "../middleware/errorHandler";
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors());
 app.get("/greetings", (_req: Request, res: Response) => {
   res.send("Hello");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
